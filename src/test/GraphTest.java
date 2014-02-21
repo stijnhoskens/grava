73826a32058d60c4417d633e20123e2da5ae01a7
stijnhoskens <stijn.hoskens@student.kuleuven.be>
2014-02-21 19:30:59 +0100

    tested graph parsing

--- src/test/GraphTest.java
+++ src/test/GraphTest.java
@@ -1,43 +1,35 @@
 package test;
 
-import static org.junit.Assert.*;
 import edge.WeightedEdge;
 import graph.Graph;
-import graph.GraphExplorer;
 import graph.parser.GraphParser;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import node.IdNode;
 
 import org.junit.Before;
-import org.junit.Test;
 
-public class GraphTest {
+public abstract class GraphTest {
 
-	private Graph<IdNode, WeightedEdge<IdNode>> graph;
+	protected Graph<IdNode, WeightedEdge<IdNode>> graph;
 
-	private final IdNode S = new IdNode("S"), A = new IdNode("A"),
+	protected final IdNode S = new IdNode("S"), A = new IdNode("A"),
 			B = new IdNode("B"), C = new IdNode("C"), D = new IdNode("D"),
 			E = new IdNode("E"), F = new IdNode("F"), G = new IdNode("G");
 
-	private Set<IdNode> nodes;
+	protected Set<IdNode> nodes;
 
 	@Before
 	public void setUp() throws Exception {
-		nodes = new HashSet<IdNode>(Arrays.asList(S, A, B, C, D, E, F, G));
+		nodes = asSet(S, A, B, C, D, E, F, G);
 		graph = GraphParser.parse("./parser/graph");
 	}
 
-	@Test
-	public void test_graphParsing() {
-		GraphExplorer<IdNode, WeightedEdge<IdNode>> explorer = new GraphExplorer<>(
-				graph, S);
-		Map<IdNode, Set<WeightedEdge<IdNode>>> map = explorer.getNodeMapping();
-		assertTrue(nodes.equals(map.keySet()));
+	public static Set<IdNode> asSet(IdNode... items) {
+		return new HashSet<IdNode>(Arrays.asList(items));
 	}
 
 }
