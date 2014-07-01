a79140ee481ac5224a910a09e5676c827757b514
Roger Vandewalle <stijn_hoskens2@hotmail.com>
2014-07-01 17:25:09 +0200

    begin on search strategies

---
+++ src/grava/search/SearchStrategy.java
@@ -0,0 +1,11 @@
+package grava.search;
+
+import grava.edge.interfaces.Link;
+import grava.graph.Graph;
+import grava.walk.Walk;
+
+public interface SearchStrategy<V, E extends Link<V>> {
+
+	Walk<V, E> findPath(Graph<V, E> graph, V start, V end);
+
+}
---
+++ src/grava/walk/Walk.java
@@ -0,0 +1,85 @@
+package grava.walk;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import grava.edge.interfaces.Link;
+
+public class Walk<V, E extends Link<V>> {
+
+	private final List<V> vertices;
+	private final List<E> edges;
+
+	public Walk(V start) {
+		vertices = new ArrayList<>();
+		edges = new ArrayList<>();
+		vertices.add(start);
+	}
+
+	public List<V> getVertices() {
+		return Collections.unmodifiableList(vertices);
+	}
+
+	public List<E> getEdges() {
+		return Collections.unmodifiableList(edges);
+	}
+
+	public V beginVertex() {
+		return vertices.get(0);
+	}
+
+	public V endVertex() {
+		return vertices.get(vertices.size() - 1);
+	}
+
+	public void extend(E e, V v) {
+		edges.add(e);
+		vertices.add(v);
+	}
+
+	public Walk<V, E> getExtended(E e, V v) {
+		Walk<V, E> walk = new Walk<>(v(), e());
+		walk.extend(e, v);
+		return walk;
+	}
+
+	public boolean isTrail() {
+		return new HashSet<>(edges).size() == edges.size();
+	}
+
+	public boolean isPath() {
+		return new HashSet<>(vertices).size() == vertices.size();
+	}
+
+	public boolean isClosed() {
+		return beginVertex().equals(endVertex());
+	}
+
+	public boolean isCircuit() {
+		return isTrail() && isClosed();
+	}
+
+	public boolean isCycle() {
+		List<V> v = v();
+		v.remove(endVertex());
+		List<E> e = e();
+		e.remove(e.size() - 1);
+		Walk<V, E> walk = new Walk<>(v, e);
+		return isClosed() && walk.isPath();
+	}
+
+	private Walk(List<V> vertices, List<E> edges) {
+		this.vertices = vertices;
+		this.edges = edges;
+	}
+
+	private List<V> v() {
+		return new ArrayList<>(vertices);
+	}
+
+	private List<E> e() {
+		return new ArrayList<>(edges);
+	}
+}
