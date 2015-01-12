dedbf9d81717b8208eea602b953bface41b5668d
Roger Vandewalle <stijn_hoskens2@hotmail.com>
2015-01-13 00:30:20 +0100

    maze already shows, still some problems

--- src/gravix/maze/MazeView.java
+++ src/gravix/maze/MazeView.java
@@ -25,14 +25,14 @@ public class MazeView extends JApplet {
 
 	@Override
 	public void init() {
-		setBackground(Color.LIGHT_GRAY);
+		setBackground(Color.WHITE);
 	}
 
 	@Override
 	public void start() {
-		maze = MazeBuilder.square(5, MazeNode::new)
+		maze = MazeBuilder.square(8, MazeNode::new)
 				.withListeners(new Listener()).matrix();
-		repaint();
+		//maze.addWallAt(new Position(0, 0), Direction.UP);
 	}
 
 	@Override
@@ -41,8 +41,8 @@ public class MazeView extends JApplet {
 	}
 
 	private Position toUpperLeftCorner(Position p) {
-		int newX = (maze.height() - p.getX() - 1) * SQUARE_WIDTH;
-		int newY = p.getY() * SQUARE_WIDTH;
+		int newY = (maze.height() - p.getY() - 1) * SQUARE_WIDTH;
+		int newX = p.getX() * SQUARE_WIDTH;
 		return new Position(newX, newY);
 	}
 
@@ -52,7 +52,24 @@ public class MazeView extends JApplet {
 	}
 
 	private void drawWall(Position p, Direction d, Graphics g) {
-		
+		g.setColor(Color.BLACK);
+		if (d.isHorizontal()) {
+			int x = d.increment() ? p.getX() + 1 : p.getX();
+			int y = p.getY();
+			Position first = toUpperLeftCorner(new Position(x, y));
+			Position second = toUpperLeftCorner(new Position(x, y - 1));
+			drawLine(first, second, g);
+		} else {
+			int x = p.getX();
+			int y = d.increment() ? p.getY() : p.getY() - 1;
+			Position first = toUpperLeftCorner(new Position(x, y));
+			Position second = toUpperLeftCorner(new Position(x + 1, y));
+			drawLine(first, second, g);
+		}
+	}
+
+	private void drawLine(Position p, Position q, Graphics g) {
+		g.drawLine(p.getX(), p.getY(), q.getX(), q.getY());
 	}
 
 	private class Listener implements MazeListener {
@@ -60,22 +77,30 @@ public class MazeView extends JApplet {
 		@Override
 		public void mazeCreated(int w, int h, boolean flag) {
 			painter = g -> {
-				g.setColor(Color.LIGHT_GRAY);
 				IntStream.range(0, w).forEach(x -> {
 					IntStream.range(0, h).forEach(y -> {
-						drawSquare(new Position(x, y), g);
+						Position p = new Position(x, y);
+						g.setColor(Color.LIGHT_GRAY);
+						drawSquare(p, g);
+						if (y == 0)
+							drawWall(p, Direction.DOWN, g);
+						if (y == h - 1)
+							drawWall(p, Direction.UP, g);
+						if (x == 0)
+							drawWall(p, Direction.LEFT, g);
+						if (x == w - 1)
+							drawWall(p, Direction.RIGHT, g);
 					});
 				});
 			};
-			repaint();
 		}
 
 		@Override
 		public void wallAdded(Position p, Direction d) {
 			painter = g -> {
-
+				drawWall(p, d, g);
+				System.out.println("painting a wall");
 			};
-			repaint();
 		}
 
 		@Override
@@ -83,7 +108,6 @@ public class MazeView extends JApplet {
 			painter = g -> {
 
 			};
-			repaint();
 		}
 	}
 
